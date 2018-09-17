434b63638f8231e350cd6e321b711c8075cd397f
Eddie956 <mutugieddie3@gmail.com>
2018-09-17 10:45:59 +0300

    f

--- config.py
+++ config.py
@@ -9,6 +9,7 @@ class Config:
     """
     SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql+psycopg2://a:mango@localhost/blog')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
     ADMIN_USERNAME = os.environ.get('ADMIN_USERNAME')
