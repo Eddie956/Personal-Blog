f49c0cbaee99e8a82c2c0d74fd74baef135cd5be
Eddie956 <mutugieddie3@gmail.com>
2018-09-17 12:09:26 +0300

    init commit

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app, db
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User, Role, Post, Comment
+from app.models import User, Role, Post, Comment, Subscribers
 from datafreeze import freeze
 
 
@@ -18,7 +18,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-	return dict(app=app, db=db, User=User, Role=Role, Post=Post, Comment=Comment)
+	return dict(app=app, db=db, User=User, Role=Role, Post=Post, Comment=Comment, Subscribers=Subscribers)
 
 
 @manager.command
